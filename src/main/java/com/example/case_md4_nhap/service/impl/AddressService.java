c502c2508d681e7c91dab81574225d916c45fd50
DatVu <revuhongdat@gmail.com>
2023-10-31 23:22:31 +0700

    Tạo findAll

--- src/main/java/com/example/case_md4_nhap/service/impl/AddressService.java
+++ src/main/java/com/example/case_md4_nhap/service/impl/AddressService.java
@@ -1,2 +1,33 @@
-package com.example.case_md4_nhap.service.impl;public class AddressService {
+package com.example.case_md4_nhap.service.impl;
+
+import com.example.case_md4_nhap.model.Address;
+import com.example.case_md4_nhap.repository.IAddressRepository;
+import com.example.case_md4_nhap.service.IAddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class AddressService implements IAddressService {
+    @Autowired
+    IAddressRepository iAddressRepository;
+    @Override
+    public Iterable<Address> findAll() {
+        return iAddressRepository.findAll();
+    }
+
+    @Override
+    public Optional<Address> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Address save(Address address) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
